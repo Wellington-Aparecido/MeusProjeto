89f6d5478a8eead131520ec16736fd5b1aeca58c
Wellington Nascimento <wellington.nascimento1994@outlook.com>
2025-02-16 11:45:32 -0300

    criação de cardapio

---
+++ HamburgueriaHery/home.js
@@ -0,0 +1,15 @@
+const btnPedir = document.querySelectorAll('.btn-pedir');
+
+btnPedir.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const pedido = btn.getAttribute('data-pedido');
+    enviarPedido(pedido);
+  });
+});
+
+function enviarPedido(pedido) {
+  // Código para enviar a mensagem para a lanchonete
+  console.log(`Pedido enviado: ${pedido}`);
+  // Exemplo de código para enviar uma mensagem via WhatsApp
+   window.open(`https://wa.me/5516997521019?text=Pedido: ${pedido}`);
+}
